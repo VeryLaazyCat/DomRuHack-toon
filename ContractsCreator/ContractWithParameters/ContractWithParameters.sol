9cd227565f3499d23c560c3a89f19cdcc5bd06ba
VeryLaazyCat <iikot860@gmail.com>
2021-09-25 17:29:24 +0300

    fix

--- ContractsCreator/ContractWithParameters/ContractWithParameters.sol
+++ ContractsCreator/ContractWithParameters/ContractWithParameters.sol
@@ -3,9 +3,6 @@ pragma solidity 0.8.7;
 
 contract ContractWithParameters {
     uint storedData;
-    constructor (uint data) {
-
-    }
 
     function set(uint x) public {
         storedData = x;
