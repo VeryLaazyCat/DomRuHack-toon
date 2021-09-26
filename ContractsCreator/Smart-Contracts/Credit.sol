38b6159d803fab66613d0ff09a35a3bf7fb566ee
Dmitry Semakov <radqqws@gmail.com>
2021-09-26 04:18:09 +0300

    Дополнен контракт заключения кредита

--- ContractsCreator/Smart-Contracts/Credit.sol
+++ ContractsCreator/Smart-Contracts/Credit.sol
@@ -3,14 +3,26 @@ pragma solidity 0.8.7;
 
 import "./Entities.sol";
 
+struct BankInfo {
+    string name;
+    uint lastNumOfDocument;
+    address bankAddress;
+}
+
 contract Credit is Borrower, Bank, Insurer {
     enum CreditStatus { Processed, Complite }
 
     CreditStatus public _status;
+    uint private _docNumber;
     int private _creditSum;
     
-    constructor() {
+    constructor(BankInfo memory representative) {
         _status = CreditStatus.Processed;
+
+        _bankName = representative.name;
+        _bankAddress = representative.bankAddress;
+
+        _docNumber = representative.lastNumOfDocument + 1;
         _creditSum = 0;
     }
 
@@ -24,22 +36,26 @@ contract Credit is Borrower, Bank, Insurer {
     // ---
     // Events
 
-    event CreditSumChanged(address indexed whoChange, int indexed newSum);
+    event CreditSumChanged(address indexed whoChange, int indexed oldSum, int indexed newSum);
     event DepositOnAccount(address indexed from, address indexed where, int depositSum);
     event ActualCreditSum(int indexed creditSum);
 
     // ---
     // Functions
 
-    function SetCreditSum(int creditSum) public onlyBank() {
-        emit CreditSumChanged(_bankAddress, creditSum);
-        _creditSum = creditSum;
+    function SetCreditSum(int newCreditSum) public onlyBank() {
+        emit CreditSumChanged(_bankAddress, _creditSum, newCreditSum);
+        _creditSum = newCreditSum;
     }
 
     function Deposit(int depositSum) public {
         emit DepositOnAccount(msg.sender, _bankAddress, depositSum);
         _creditSum -= depositSum;
         emit ActualCreditSum(_creditSum);
+    }
+
+    function GetCreditSum() external view returns (int) {
+        return _creditSum;
     }
 
     // ---
